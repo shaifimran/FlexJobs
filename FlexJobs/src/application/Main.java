b476d20958218ef2a831fe7ce101ec7e233ba5a6
Abdul Wasay <abdulwasayisl27@gmail.com>
2024-11-22 00:35:26 +0500

    Student Login with Frontend validation

--- FlexJobs/src/application/Main.java
+++ FlexJobs/src/application/Main.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,10 +11,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	public String connectionString = "jdbc:mysql://localhost:3306/flexjobs";
+	public String userName = "root";
+	public String password = "wasay";
+	Connection connection;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("./UI/ChatBox.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("./UI/StudentLogin.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("./UI/application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -18,6 +27,19 @@ public class Main extends Application {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public Connection getConnection() {
+		try {
+			if (connection == null || connection.isClosed()) {
+				connection = DriverManager.getConnection(connectionString, userName, password);
+			}
+		} catch (SQLException e) {
+			System.err.println("Error creating connection: " + e.getMessage());
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		return connection;
 	}
 
 	public static void main(String[] args) {
